6e16d7509074abc042c9f6891b5b842a43290da7
andyoung1226 <andyoung1226@gmail.com>
2022-06-28 19:56:01 +0900

    asdfa

--- cnn_classifi.py
+++ cnn_classifi.py
@@ -27,8 +27,8 @@ class imu_classification():
         l_a_y = lin_acc.y
         l_a_z = lin_acc.z
 
-    #def reset_imudata(self):
-    #    self.imu_data = np.empty(shape=(6,50))
+    def reset_imudata(self):
+        self.imu_data = np.empty(shape=(6,50))
 
         self.imu_data = np.append(self.imu_data, np.array([[a_v_x, a_v_y, a_v_z, l_a_x, l_a_y, l_a_z]]), axis=0)
         if len(self.imu_data) == 50:
@@ -41,7 +41,7 @@ class imu_classification():
                 print("normal driving")
             else:
                 print("vibaration detected")
-                #self.reset_imudata()
+                self.reset_imudata()
             self.imu_data = np.delete(self.imu_data, (0), axis=0)
 
 if __name__ == "__main__":
