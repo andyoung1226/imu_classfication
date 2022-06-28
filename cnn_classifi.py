77033767764343279db504432e1e575eae19e10c
andyoung1226 <andyoung1226@gmail.com>
2022-06-28 18:08:30 +0900

    qaq

--- cnn_classifi.py
+++ cnn_classifi.py
@@ -16,7 +16,6 @@ class imu_classification():
         self.imu_sub = rospy.Subscriber('/imu', Imu, self.imu_callback, queue_size=1)
         self.classifi_pub = rospy.Publisher('/cnn_classfi', Int16, queue_size=1)
         self.imu_data = np.empty(shape=(0, 6))
-        self.cnn_model = models.load_model('/home/wheelchair/catkin_ws/src/imu_classfication/model/cnn_model')
 
     def imu_callback(self, msg):
         ang_vel = msg.angular_velocity
@@ -32,13 +31,13 @@ class imu_classification():
         if len(self.imu_data) == 50:
             print(self.imu_data.shape, "imu shape")
             predict_data = self.imu_data.reshape(50, 6, 1)
-            cnnmodel = self.cnn_model
-            prediction = cnnmodel.predict(predict_data)
+            prediction = cnn_model.predict(predict_data)
             print(prediction)
             #self.classifi_pub.publsih(prediction)
             self.imu_data = np.delete(self.imu_data, (0), axis=0)
 
 if __name__ == "__main__":
+    cnn_model = models.load_model('/home/wheelchair/catkin_ws/src/imu_classfication/model/cnn_model')
     os.chdir("/home/wheelchair/catkin_ws/src/imu_classfication/model")
     rospy.init_node("imu_cnn")
     cnn = imu_classification()
