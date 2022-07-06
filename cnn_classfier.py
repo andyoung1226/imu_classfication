436c289fa0d7e7793dbae30cdd6644f5bc311f10
andyoung1226 <andyoung1226@gmail.com>
2022-07-06 18:25:29 +0900

    ff

--- cnn_classfier.py
+++ cnn_classfier.py
@@ -15,9 +15,28 @@ class imu_classification():
     def __init__(self):
         self.imu_sub = rospy.Subscriber('/data', Float32MultiArray, self.data_callback, queue_size=1)
         self.classifi_pub = rospy.Publisher('/cwt_cnn_classfi', Int16, queue_size=1)
+  
+    def create_cwt_images(self, X, n_scales, n_samples, wavelet_name="morl"):
+        # n_samples = X.shape[0]
+        n_signals = X.shape[2]
+        n_times = X.shape[1]
+
+        scales = np.arange(1, n_scales + 1)
+        X_cwt = np.ndarray(shape=(n_samples, n_scales, n_times, n_signals), dtype='float32')
+
+        for sample in range(n_samples):
+            for signal in range(n_signals):
+                serie = X[sample, :, signal]
+                coeffs, freqs = pywt.cwt(serie, scales, wavelet_name)
+                X_cwt[sample, :, :, signal] = coeffs
+        
+        self.cwt_data = X_cwt
 
     def data_callback(self, msg):
-        predict_data = msg
+        to_cwt_data = np.array(msg.data)
+        to_cwt_data = predict_data.reshape(1, 50, 6)
+        self.create_cwt_images(to_cwt_data, 32, 1)
+        predict_data = self.cwt_data
         prediction = cnn_model.predict(predict_data)
         predict_index = int(np.argmax(prediction[0]))
         self.classifi_pub.publish(predict_index)
