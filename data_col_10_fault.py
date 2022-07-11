b88f481f444e823b2376dff0a019e6ba00b92aa0
andyoung1226 <andyoung1226@gmail.com>
2022-07-12 00:30:19 +0900

    Q

--- data_col_10_fault.py
+++ data_col_10_fault.py
@@ -3,12 +3,14 @@ import rospy
 import scipy.io
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import Quaternion, Vector3
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Int8
 import numpy as np
 
 data_x = np.empty((0,1), float)
 data_y = np.empty((0,1), float)
 data_z = np.empty((0,1), float)
+a = 1
+data_col = False
 normal = np.zeros((10000,1), dtype=float)
 normal_dic = {"imu_normal_index": normal}
 scipy.io.savemat('normal_index.mat', normal_dic)
@@ -18,7 +20,7 @@ fault_dic = {"imu_normal_index": fault}
 scipy.io.savemat('fault_index.mat', fault_dic)
 
 def imu_callback(msg):
-        global data_x, data_y, data_z, a
+        global data_x, data_y, data_z, a, data_col
         #angular_velocity = msg.angular_velocity
         linear_acceleration = msg.linear_acceleration
         #a_v_x = angular_velocity.x
@@ -28,23 +30,35 @@ def imu_callback(msg):
         l_a_y = linear_acceleration.y
         l_a_z = linear_acceleration.z
 
-        data_x = np.append(data_x, np.array([l_a_x]))
-        data_y = np.append(data_y, np.array([l_a_y]))
-        data_z = np.append(data_z, np.array([l_a_z]))
+        if data_col == True:
+                data_x = np.append(data_x, np.array([l_a_x]))
+                data_y = np.append(data_y, np.array([l_a_y]))
+                data_z = np.append(data_z, np.array([l_a_z]))
 
-        if len(data_x) == 50000:
-                data_x = data_x.reshape(500, 10, 10, 1)
-                data_y = data_y.reshape(500, 10, 10, 1)
-                data_z = data_z.reshape(500, 10, 10, 1)
+        if len(data_x) == 100:
+                data_x = data_x.reshape(1, 10, 10, 1)
+                data_y = data_y.reshape(1, 10, 10, 1)
+                data_z = data_z.reshape(1, 10, 10, 1)
                 data = np.concatenate((data_x, data_y, data_z), axis=3)
                 print(data.shape)
-                data_dic = {"imu_normal": data}
-                scipy.io.savemat('imu_normal.mat', data_dic)
-                print("data_saved")
+                data_dic = {"imu_fault": data}
+                scipy.io.savemat('./fault_data/imu_fault_{0}.mat'.format(a), data_dic)
+                a += 1
+                print("data_saved", a)
+                data_x = np.empty((0,1), float)
+                data_y = np.empty((0,1), float)
+                data_z = np.empty((0,1), float)
 
+def keyCB(msg):
+        global data_col
+        if msg.data == 1:
+                data_col = True
+        else:
+                data_col = False
 def listener():
         rospy.init_node('imudata_mat', anonymous=True)
         rospy.Subscriber("/imu", Imu, imu_callback)
+        rospy.Subscriber("/keyboard", Int8, keyCB)
         rospy.spin()
 
 if __name__=='__main__':
